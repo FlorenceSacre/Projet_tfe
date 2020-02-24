ec3061ba604e4ea14dea4929635ac0bbd0dc294c
Florence <florencesacre@yahoo.fr>
2020-02-24 14:51:11 +0100

    Migrations & form user

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -8,18 +8,57 @@
             <div class="panel-body">
                 <div class="col-sm-12">
                     {!! Form::model($user, ['route' => ['user.update', $user->id], 'method' => 'put', 'class' => 'form-horizontal panel']) !!}
-                    <div class="form-group {!! $errors->has('name') ? 'has-error' : '' !!}">
-                        {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Nom']) !!}
-                        {!! $errors->first('name', '<small class="help-block">:message</small>') !!}
+                    <div class="form-group {!! $errors->has('username') ? 'has-error' : '' !!}">
+                        {!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'Pseudo']) !!}
+                        {!! $errors->first('username', '<small class="help-block">:message</small>') !!}
+                    </div>
+                    <div class="form-group {!! $errors->has('lastname') ? 'has-error' : '' !!}">
+                        {!! Form::text('lastname', null, ['class' => 'form-control', 'placeholder' => 'Nom']) !!}
+                        {!! $errors->first('lastname', '<small class="help-block">:message</small>') !!}
+                    </div>
+                    <div class="form-group {!! $errors->has('firstname') ? 'has-error' : '' !!}">
+                        {!! Form::text('firstname', null, ['class' => 'form-control', 'placeholder' => 'Prénom']) !!}
+                        {!! $errors->first('firstname', '<small class="help-block">:message</small>') !!}
+                    </div>
+                    <label>Genre</label>
+                    <div class="form-group">
+                        <div class="checkbox">
+                            <label>
+                                {!! Form::checkbox('male', 1, null) !!} Homme<br />
+                                {!! Form::checkbox('female', 2, null) !!} Femme
+                            </label>
+                        </div>
+                    </div>
+                    <label>Date d'anniversaire</label>
+                    <div class="form-group {!! $errors->has('birthday') ? 'has-error' : '' !!}">
+                        {!! Form::date('birthday', null, ['class' => 'form-control']) !!}
+                        {!! $errors->first('birthday', '<small class="help-block">:message</small>') !!}
                     </div>
                     <div class="form-group {!! $errors->has('email') ? 'has-error' : '' !!}">
                         {!! Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Email']) !!}
                         {!! $errors->first('email', '<small class="help-block">:message</small>') !!}
                     </div>
+                    <div class="form-group {!! $errors->has('password') ? 'has-error' : '' !!}">
+                        {!! Form::password('password', ['class' => 'form-control', 'placeholder' => 'Mot de passe']) !!}
+                        {!! $errors->first('password', '<small class="help-block">:message</small>') !!}
+                    </div>
+                    <div class="form-group">
+                        {!! Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Confirmation mot de passe']) !!}
+                    </div>
+                    <label>Role</label>
                     <div class="form-group">
                         <div class="checkbox">
                             <label>
-                                {!! Form::checkbox('admin', 1, null) !!}Administrateur
+                                {!! Form::checkbox('customer', 1, null) !!} Client<br />
+                                {!! Form::checkbox('provider', 2, null) !!} Fournisseur<br />
+                                {!! Form::checkbox('customer and provider', 3, null) !!} Client et fournisseur
+                            </label>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <div class="checkbox">
+                            <label>
+                                {!! Form::checkbox('admin', 1, null) !!} Administrateur
                             </label>
                         </div>
                     </div>
